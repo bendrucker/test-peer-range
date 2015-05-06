ef7c9f14157fb120f1207de0b80eca77707eed50
Ben Drucker <bvdrucker@gmail.com>
2015-05-06 12:39:49 -0400

    Initial implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 'use strict'
 
-import majors from 'major-versions'
+import majorVersions from 'major-versions'
 import array from 'ensure-array'
-import originalNpm from 'npm'
+import npm from 'npm'
 import Promise from 'bluebird'
+import {cwd} from 'process'
+import {resolve} from 'path'
+import assert from 'assert'
+const pkg = require(resolve(cwd(), 'package.json'))
 
-const npm = Promise.promisifyAll(npm)
+Promise.promisifyAll(npm)
+
+export default testPeer ()
+
+function majors (name, range) {
+  assert(name, 'Peer name must be defined')
+  assert(range, 'Peer range must be defined')
+  return npm.viewAsync([name, 'versions.latest'])
+    .then(v => majorVersions(range, v))
+}
+
+function run (script) {
+  return npm.runScriptAsync(script)
+}
+
+function install (name, version) {
+  return npm.installAsync([`${name}@${version}`])
+}
+
+function uninstall (name) {
+  return npm.uninstallAsync(name)
+}
