f11eb2fa19df26135c2fe06f017a5188f853b275
Ben Drucker <bvdrucker@gmail.com>
2015-05-08 12:52:00 -0700

    Convert CLI for class

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,18 @@
 'use strict'
 
-import test from '../'
+import {Runner} from '../'
 import yargs from 'yargs'
 import prop from 'dot-prop'
 import {resolve} from 'path'
-import {cwd} from 'process'
 
+const {cwd} = process
 const {argv} = yargs.demand(1).default('script', 'test-main')
 const [peer] = argv._
 const {script} = argv
 const pkg = require(resolve(cwd(), 'package.json'))
 const range = prop.get(pkg, `peerDependencies.${peer}`)
 
-test(peer, range, script).catch(fail)
+new Runner(peer, range, {script}).run().catch(fail)
 
 function fail (err) {
   console.error(err)
