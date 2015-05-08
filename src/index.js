c13f6490fce862a5a3da022a4d2fcc383b418e8f
Ben Drucker <bvdrucker@gmail.com>
2015-05-08 11:29:26 -0700

    Rewrite API as an evented class

--- src/index.js
+++ src/index.js
@@ -8,41 +8,50 @@ import {cwd} from 'process'
 import {resolve} from 'path'
 import assert from 'assert'
 import {partial} from 'ap'
+import {EventEmitter} from 'events'
 
-export default function testPeer (name, range, script) {
-  return load()
-    .return([name, range])
-    .spread(majors)
-    .each(partial(run, name, script))
-}
-
-function majors (name, range) {
-  assert(name, 'Peer name must be defined')
-  assert(range, 'Peer range must be defined')
-  return promisify(npm.commands.view)([name, 'dist-tags.latest'])
-    .then(v => majorVersions(range, v))
+export class Runner extends EventEmitter {
+  constructor (name, range, script = 'test-main') {
+    assert(name, 'Peer name must be defined')
+    assert(range, 'Peer range must be defined')
+    Object.assign(this, {name, range, script})
+  }
+  versions () {
+    return promisify(npm.commands.view)([name, 'dist-tags.latest'])
+      .then(v => majorVersions(range, v))
+      .tap((versions) => this.emit('versions', versions))
+  }
+  install (version = '') {
+    this.emit('preinstall', version)
+    return promisify(npm.commands.install)([`${this.name}@${version}`])
+      .then(() => this.emit('postinstall', version))
+      .return(version)
+  }
+  uninstall () {
+    this.emit('preuninstall', version)
+    return promisify(npm.commands.uninstall)(this.name)
+      .then(() => this.emit('postuninstall', version))
+  }
+  runScript (version) {
+    this.emit('prescript', version)
+    return promisify(npm.commands.runScript)([this.script])
+      .then(() => this.emit('postscript', version))
+  }
+  test (version) {
+    return this.install(version)
+      .bind(this)
+      .then(this.runScript)
+      .then(this.uninstall)
+  }
+  run () {
+    return load()
+      .bind(this)
+      .then(this.versions)
+      .each(this.test)
+      .then(() => this.install)
+  }
 }
 
 function load () {
   return promisify(npm.load)()
-}
-
-function run (name, script, version) {
-  return install(name, version)
-    .return(script)
-    .then(runScript)
-    .return(name)
-    .then(uninstall)
-}
-
-function install (name, version) {
-  return promisify(npm.commands.install)([`${name}@${version}`])
-}
-
-function runScript (name) {
-  return promisify(npm.commands.runScript)([name])
-}
-
-function uninstall (name) {
-  return promisify(npm.commands.uninstall)(name)
 }
