3d1a2c4dfb37e205245018fd4d78ed5aba05679d
Ben Drucker <bvdrucker@gmail.com>
2015-05-08 10:27:45 -0700

    Call npm properly

--- src/cli.js
+++ src/cli.js
@@ -2,13 +2,20 @@
 
 import test from '../'
 import yargs from 'yargs'
+import prop from 'dot-prop'
+import {resolve} from 'path'
+import {cwd} from 'process'
 
-const {argv} = yargs.demand(1)
+const {argv} = yargs.demand(1).default('script', 'test-main')
 const [peer] = argv._
+const {script} = argv
+const pkg = require(resolve(cwd(), 'package.json'))
+const range = prop.get(pkg, `peerDependencies.${peer}`)
 
-test(peer).catch(fail)
+test(peer, range, script).catch(fail)
 
 function fail (err) {
-  console.err(err)
+  console.error(err)
+  console.error(err.stack)
   process.exit(1)
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@
 import majorVersions from 'major-versions'
 import array from 'ensure-array'
 import npm from 'npm'
-import Promise from 'bluebird'
+import {promisify} from 'bluebird'
 import {cwd} from 'process'
 import {resolve} from 'path'
 import assert from 'assert'
 import {partial} from 'ap'
-const pkg = require(resolve(cwd(), 'package.json'))
-
-Promise.promisifyAll(npm)
 
 export default function testPeer (name, range, script) {
-  return majors(name, range).each(partial(run, name, script))
+  return load()
+    .return([name, range])
+    .spread(majors)
+    .each(partial(run, name, script))
+}
+
+function majors (name, range) {
+  assert(name, 'Peer name must be defined')
+  assert(range, 'Peer range must be defined')
+  return promisify(npm.commands.view)([name, 'dist-tags.latest'])
+    .then(v => majorVersions(range, v))
+}
+
+function load () {
+  return promisify(npm.load)()
 }
 
 function run (name, script, version) {
@@ -24,21 +35,14 @@ function run (name, script, version) {
     .then(uninstall)
 }
 
-function majors (name, range) {
-  assert(name, 'Peer name must be defined')
-  assert(range, 'Peer range must be defined')
-  return npm.viewAsync([name, 'dist-tags.latest'])
-    .then(v => majorVersions(range, v))
+function install (name, version) {
+  return promisify(npm.commands.install)([`${name}@${version}`])
 }
 
 function runScript (name) {
-  return npm.runScriptAsync(name)
-}
-
-function install (name, version) {
-  return npm.installAsync([`${name}@${version}`])
+  return promisify(npm.commands.runScript)([name])
 }
 
 function uninstall (name) {
-  return npm.uninstallAsync(name)
+  return promisify(npm.commands.uninstall)(name)
 }
