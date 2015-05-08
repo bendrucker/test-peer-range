b9c0de2044d5a7e0f74fede3a754862ee5bcdde8
Ben Drucker <bvdrucker@gmail.com>
2015-05-08 15:41:49 -0700

    Use runner events to log test steps

--- src/cli.js
+++ src/cli.js
@@ -4,18 +4,44 @@ import {Runner} from '../'
 import yargs from 'yargs'
 import prop from 'dot-prop'
 import {resolve} from 'path'
+import {createLogger} from 'clout'
 
 const {cwd} = process
-const {argv} = yargs.demand(1).default('script', 'test-main')
-const [peer] = argv._
-const {script} = argv
 const pkg = require(resolve(cwd(), 'package.json'))
+const log = createLogger(pkg.name)
+
+const {argv} = yargs
+  .demand(1)
+  .option('script', {
+    default: 'test-main',
+    describe: 'test script'
+  })
+const [peer] = argv._
+const {script, verbose} = argv
 const range = prop.get(pkg, `peerDependencies.${peer}`)
 
-new Runner(peer, range, {script}).run().catch(fail)
+new Runner(peer, range, {script})
+  .on('versions', (versions) => {
+    log(`Testing ${peer} versions ${versions.join(', ')}`)
+  })
+  .on('preinstall', (version) => {
+    log(`Installing ${peer}@${version}`)
+  })
+  .on('postinstall', (version) => {
+    log(`Installed ${peer}@${version}`)
+  })
+  .on('prescript', (version) => {
+    log(`Starting "${script}" for ${peer}@${version}`)
+  })
+  .on('result', ({passed, version}) => {
+    const outcome = passed ? 'Passed' : 'Failed'
+    log(`${outcome}: ${peer}@${version}`)
+  })
+  .run()
+  .catch(fail)
 
 function fail (err) {
-  console.error(err)
+  log.error(err)
   console.error(err.stack)
   process.exit(1)
 }
